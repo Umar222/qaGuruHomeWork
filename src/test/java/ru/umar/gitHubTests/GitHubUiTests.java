2fe5c9a655031bcc10e47431d79d21ba9c5e7be2
Umar222 <rabotatest1992@mail.ru>
2021-11-08 16:28:55 +0300

    Тест DragAndDrop вывел в отдельный класс

--- src/test/java/ru/umar/gitHubTests/GitHubUiTests.java
+++ src/test/java/ru/umar/gitHubTests/GitHubUiTests.java
@@ -1,6 +1,5 @@
 package ru.umar.gitHubTests;
 
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -16,19 +15,9 @@ public class GitHubUiTests {
         open("https://github.com/selenide/selenide");
         $(By.id("wiki-tab")).click();
         $(By.id("wiki-body")).shouldHave(text("Welcome to the selenide wiki!"));
-        $(By.xpath("//button[@class=\"f6 Link--muted js-wiki-more-pages-link btn-link mx-auto\"]")).click();
-        $(By.xpath("//div[@class=\"Box Box--condensed color-shadow-small\"]")).shouldHave(text("SoftAssertions"));
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
         $(By.xpath("//a[@href=\"/selenide/selenide/wiki/SoftAssertions\"]")).click();
         $(By.className("markdown-body")).shouldHave(text("Using JUnit5 extend test class:"));
     }
-
-    @Test
-    void dragAndDropTest(){
-        SelenideElement elementA = $(By.id("column-a"));
-        SelenideElement elementB = $(By.id("column-b"));
-        open("https://the-internet.herokuapp.com/drag_and_drop");
-        $(elementA).dragAndDropTo(elementB);
-    }
-
-
 }
