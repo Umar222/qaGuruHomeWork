8f34965cef310bbf2498fde6a536bd2a6e78845d
Umar222 <rabotatest1992@mail.ru>
2021-11-09 19:03:02 +0300

    Класс для тестовых данных

---
+++ src/main/java/ru/TestData.java
@@ -0,0 +1,14 @@
+package ru;
+
+import com.github.javafaker.Faker;
+
+public class TestData {
+
+   static Faker faker = new Faker();
+
+    public static String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            phoneNumber = faker.phoneNumber().cellPhone(),
+            address = faker.address().streetAddress();
+}
