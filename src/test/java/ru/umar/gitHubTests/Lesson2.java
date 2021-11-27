e69d6d6ac15c1f1f7b220faa5bd28f299ee0454c
Umar222 <rabotatest1992@mail.ru>
2021-11-27 22:54:30 +0300

    Добавил провальный тест

--- src/test/java/ru/umar/gitHubTests/Lesson2.java
+++ src/test/java/ru/umar/gitHubTests/Lesson2.java
@@ -15,7 +15,7 @@ public class Lesson2 extends TestBase {
 
     @Test
     @Tag("smoke")
-    public void formFilling() {
+    void formFilling() {
         open("https://demoqa.com/automation-practice-form");
         registration
                 .setFio(firstName, lastName)
@@ -38,6 +38,38 @@ public class Lesson2 extends TestBase {
                 .checkResultsTableValue("Gender", "Male")
                 .checkResultsTableValue("Mobile", "9539323424")
                 .checkResultsTableValue("Date of Birth", "01 January,1919")
+                .checkResultsTableValue("Subjects", "English")
+                .checkResultsTableValue("Hobbies", "Sports")
+                .checkResultsTableValue("Picture", "picture1.jpg")
+                .checkResultsTableValue("Address", address)
+                .checkResultsTableValue("State and City", "NCR Delhi");
+    }
+
+    @Test
+    @Tag("smoke")
+    void formFillingSpecialFiledTest() {
+        open("https://demoqa.com/automation-practice-form");
+        registration
+                .setFio(firstName, lastName)
+                .emailField(email)
+                .chooseGender("Male")
+                .userPhoneNumberField("9539323424")
+                .subjectsContainerField("English")
+                .addDateOfBirth("01", "January", "1919")
+                .hobbiesCheckbox("Sports")
+                .uploadPicture("picture1.jpg")
+                .addCurrentAddress(address)
+                .chooseStateField("NCR")
+                .chooseCityField("Delhi")
+                .submit();
+
+        registration.checkTableTitle("Thanks for submitting the form");
+
+        registration.checkResultsTableValue("Student Name", firstName +" "+ lastName)
+                .checkResultsTableValue("Student Email", email)
+                .checkResultsTableValue("Gender", "Male")
+                .checkResultsTableValue("Mobile", "9539323424")
+                .checkResultsTableValue("Date of Birth", "01 January,1929")
                 .checkResultsTableValue("Subjects", "English")
                 .checkResultsTableValue("Hobbies", "Sports")
                 .checkResultsTableValue("Picture", "picture1.jpg")
