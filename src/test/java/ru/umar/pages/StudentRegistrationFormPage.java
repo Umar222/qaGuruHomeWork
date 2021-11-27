bb124cce9d1a13782078227b3b5f2a440d3eb4d3
Umar222 <rabotatest1992@mail.ru>
2021-11-27 22:33:38 +0300

    Добавил аннотации @Step к методам

--- src/test/java/ru/umar/pages/StudentRegistrationFormPage.java
+++ src/test/java/ru/umar/pages/StudentRegistrationFormPage.java
@@ -2,6 +2,7 @@ package ru.umar.pages;
 
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import java.io.File;
@@ -30,49 +31,58 @@ public class StudentRegistrationFormPage {
         return this;
     }
 
+    @Step("Ввести адрес проживания {address}")
     public StudentRegistrationFormPage addCurrentAddress(String address) {
         currentAddress.setValue(address);
         return this;
     }
 
+    @Step("Выбрать пол {genderType}")
     public StudentRegistrationFormPage chooseGender(String genderType) {
         $(By.xpath("//label[text() = \"" + genderType + "\"]")).click();
         return this;
     }
 
+    @Step("Выбрать хобби {hobbiesType}")
     public StudentRegistrationFormPage hobbiesCheckbox(String hobbiesType) {
         $(By.xpath("//label[contains(.,'" + hobbiesType + "')]")).click();
         return this;
     }
 
+    @Step("Выбрать город {city}")
     public StudentRegistrationFormPage chooseStateField(String city) {
         $(By.id("state")).scrollTo().click();
         $(By.xpath("//div[@class = \" css-11unzgr\"]/div[text() = \"" + city + "\"]")).click();
         return this;
     }
 
+    @Step("Выбрать Штат {state}")
     public StudentRegistrationFormPage chooseCityField(String state) {
         $(By.id("city")).click();
         $(By.xpath("//div[@id=\"city\"]//div[text() = \"" + state + "\"]")).click();
         return this;
     }
 
+    @Step("Ввести имя {firstName} и отчество {lastName}")
     public StudentRegistrationFormPage setFio(String firstName, String lastName) {
         firstNameField.setValue(firstName);
         lastNameField.setValue(lastName);
         return this;
     }
 
+    @Step("Ввести почту {email}")
     public StudentRegistrationFormPage emailField(String email) {
         emailField.setValue(email);
         return this;
     }
 
+    @Step("Ввести номер телефона {userNumber}")
     public StudentRegistrationFormPage userPhoneNumberField(String userNumber) {
         userPhoneNumberField.setValue(userNumber);
         return this;
     }
 
+    @Step("Ввести дату рождения {day}, {month}, {year}")
     public StudentRegistrationFormPage addDateOfBirth(String day, String month, String year) {
         $(By.id("dateOfBirthInput")).click();
         $(By.cssSelector(".react-datepicker__month-select")).click();
@@ -82,7 +92,7 @@ public class StudentRegistrationFormPage {
         $(By.cssSelector(".react-datepicker__day--0" + day)).click();
         return this;
     }
-
+    @Step("Выбрать предмет {subjectsContainer}")
     public StudentRegistrationFormPage subjectsContainerField(String subjectsContainer) {
         someButton.click();
         $(By.id("subjectsInput")).setValue(subjectsContainer);
@@ -90,12 +100,14 @@ public class StudentRegistrationFormPage {
         return this;
     }
 
+    @Step("Загрузить изображение {fileName}")
     public StudentRegistrationFormPage uploadPicture(String fileName) {
         uploadFileField.uploadFile(new File("src/test/resources/" + fileName));
         uploadFileField.uploadFromClasspath(fileName);
         return this;
     }
 
+    @Step("Отправить форму")
     public StudentRegistrationFormPage submit() {
         submitButton.click();
         return this;
