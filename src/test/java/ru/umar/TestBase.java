3737c5b4b64b8a22ce39c0979518b9bea1cbf7e6
Umar222 <rabotatest1992@mail.ru>
2021-11-29 12:11:15 +0300

    Добавил класс owner

--- src/test/java/ru/umar/TestBase.java
+++ src/test/java/ru/umar/TestBase.java
@@ -3,16 +3,25 @@ package ru.umar;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.umar.config.CredentialsConfig;
+import ru.umar.helper.Attach;
 
 public class TestBase {
+
+    public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+    static String login = credentials.login();
+    static String password = credentials.password();
+    static String selenoidUrl = credentials.urlType();
+    static String remoteUrl = credentials.URL(login, password, selenoidUrl);
+
 
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = remoteUrl;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
